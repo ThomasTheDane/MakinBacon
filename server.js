aa66929bdbc9a1dc229acb5b103620a6c0c2caa6
thomasnat1 <Thomas.nattestad@students.ollin.edu>
2014-03-05 17:37:53 -0500

    starting workon server

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var port = process.env.PORT || 3000;
+server.listen(port);
+
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 
@@ -6,4 +12,9 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function callback () {
 	
+});
+
+app.get('/', function (req, res){
+	
+	res.send('dickbutt');
 });
