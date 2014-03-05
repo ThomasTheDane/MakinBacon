454f4b27a1f2e5f32d72ce828579f9982877bd4d
thomasnat1 <Thomas.nattestad@students.ollin.edu>
2014-03-05 16:07:07 -0500

    made script to read text files and setup models

--- models.js
+++ models.js
@@ -1,1 +1,11 @@
 var mongoose = require('mongoose');
+
+var ActorSchema = mongoose.Schema({
+    name: String,
+    movies: [String]
+});
+
+var MovieSchema = mongoose.Schema({
+	name: String,
+	actors: [String]
+});
